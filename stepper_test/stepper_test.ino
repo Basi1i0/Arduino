c010dd5de40f92b5acec7af1a7b83567a5e8e4a8
Basi1i0 <oldbasilio@gmail.com>
2020-01-18 23:52:42 -0800

    limits added

--- stepper_test/stepper_test.ino
+++ stepper_test/stepper_test.ino
@@ -1,5 +1,18 @@
-#include <Stepper.h>
+
 #include <SerialCommand.h>
+
+//
+struct stepper_t
+{
+  const int stepPin;
+  const int dirPin;
+  const int enPin;
+  unsigned int delayUsec;
+
+  long currentPos;
+  long startLim;
+  long endLim;
+};
 
 // defines pins numbers
 const int stepPin = 7; 
@@ -8,6 +21,9 @@ const int enPin = 9;
 
 const int delay_usec = 200;
 
+
+stepper_t stepperMotor = {.stepPin = 7, .dirPin = 3, .enPin = 9, .delayUsec = 200,
+                          .currentPos = 0, .startLim = 0, .endLim = 120000};
 
 #define arduinoLED 13   // Arduino LED on board
 
@@ -37,6 +53,13 @@ void setup() {
   sCmd.addCommand("HELLO", sayHello);        // Echos the string argument back
   sCmd.addCommand("P",     processCommand);  // Converts two arguments to integers and echos them back
   sCmd.addCommand("MOVE",  processMove);  
+  sCmd.addCommand("MOVETO",  processMoveTo);  
+  sCmd.addCommand("POS",  SendCurrentPos);  
+  sCmd.addCommand("END",  SendEndLim);  
+  sCmd.addCommand("START",  SendStartLim);
+  sCmd.addCommand("SETEND",  SetEndLim);  
+  sCmd.addCommand("SETSTART",  SetStartLim);  
+  sCmd.addCommand("MOVE",  processMove);  
   sCmd.setDefaultHandler(unrecognized);      // Handler for command that isn't matched  (says "What?")
   Serial.println("Ready");
   
@@ -47,28 +70,31 @@ void loop() {
    
 
   sCmd.readSerial();
- /* Move(3200, delay_usec);
-  delay(1000);
-  Move(-3200, delay_usec);
-  delay(1000);
-  */
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////
 void Move(long nsteps){
+
+  if(stepperMotor.currentPos + nsteps > stepperMotor.endLim ||
+     stepperMotor.currentPos + nsteps < stepperMotor.startLim){
+    return;
+  }
+  
+  stepperMotor.currentPos += nsteps;
+  
   if(nsteps < 0){
-    digitalWrite(dirPin,LOW); 
+    digitalWrite(stepperMotor.dirPin,LOW); 
     nsteps = -nsteps;
     }
   else
     digitalWrite(dirPin,HIGH); 
   
   for(long i = 0; i < nsteps; i++){
-    digitalWrite(stepPin,HIGH); 
-    delayMicroseconds(delay_usec); 
-    digitalWrite(stepPin,LOW); 
-    delayMicroseconds(delay_usec); 
+    digitalWrite(stepperMotor.stepPin,HIGH); 
+    delayMicroseconds(stepperMotor.delayUsec); 
+    digitalWrite(stepperMotor.stepPin,LOW); 
+    delayMicroseconds(stepperMotor.delayUsec); 
   }
 }
 
@@ -85,16 +111,66 @@ void LED_off() {
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////
-void sayHello() {
+void SendCurrentPos() {
+  Serial.println(stepperMotor.currentPos);
+}
+
+void SendEndLim() {
+  Serial.println(stepperMotor.endLim);
+}
+
+void SendStartLim() {
+  Serial.println(stepperMotor.startLim);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+void SetStartLim() {
+  long newStartLim;
   char *arg;
-  arg = sCmd.next();    // Get the next argument from the SerialCommand object buffer
-  if (arg != NULL) {    // As long as it existed, take it
-    Serial.print("Hello ");
-    Serial.println(arg);
+
+  arg = sCmd.next();
+  if (arg != NULL) {
+    newStartLim = atol(arg);    // Converts a char string to an integer
+    if (newStartLim < stepperMotor.endLim)
+      stepperMotor.startLim = newStartLim;
+    else{
+      Serial.println("ERR");
+      Serial.println("START should be smaller than END");
+      }
   }
   else {
-    Serial.println("Hello, whoever you are");
+    stepperMotor.startLim = stepperMotor.currentPos;
   }
+  
+  
+  Serial.print("startLim is ");
+  Serial.println(stepperMotor.startLim);
+  Serial.println("DONE");
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+void SetEndLim() {
+  long newEndLim;
+  char *arg;
+
+  arg = sCmd.next();
+  if (arg != NULL) {
+    newEndLim = atol(arg);    // Converts a char string to an integer
+    if (newEndLim > stepperMotor.endLim)
+      stepperMotor.endLim = newEndLim;
+    else{
+      Serial.println("ERR");
+      Serial.println("END should be greater than START");
+    }
+  }
+  else {
+    stepperMotor.endLim = stepperMotor.currentPos;
+  }
+  
+  
+  Serial.print("endLim is ");
+  Serial.println(stepperMotor.endLim);
+  Serial.println("DONE");
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////
@@ -132,28 +208,72 @@ void processMove() {
   arg = sCmd.next();
   if (arg != NULL) {
     nsteps = atol(arg);    // Converts a char string to an integer
-    
-    Serial.print("Moving ");
-    Serial.print(nsteps);
-    Serial.println(" steps");
 
-    
-    Move(nsteps);
+    if(stepperMotor.currentPos + nsteps > stepperMotor.endLim ||
+       stepperMotor.currentPos + nsteps < stepperMotor.startLim){
+      Serial.println("ERR");
+      Serial.println("Result should be within limits");
+    }
+    else{
+      Serial.print("Moving ");
+      Serial.print(nsteps);
+      Serial.println(" steps");
+  
+      Move(nsteps);
+    }
   }
   else {
     Serial.println("ERR");
     Serial.println("No arguments");
   }
+  
+  Serial.print("Current position ");
+  Serial.println(stepperMotor.currentPos);
+  Serial.println("DONE");
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+void processMoveTo() {
+  long dest;
+  char *arg;
 
   arg = sCmd.next();
   if (arg != NULL) {
-    Serial.println("ERR");
-    Serial.println("too many arguments");
-  }
+    dest = atol(arg);    // Converts a char string to an integer
 
+    if(dest > stepperMotor.endLim ||
+       dest < stepperMotor.startLim){
+      Serial.println("ERR");
+      Serial.println("Result should be within limits");
+    }
+    else{
+      Serial.print("Moving to ");
+      Serial.println(dest);
   
+      Move(dest - stepperMotor.currentPos);
+    }
+  }
+  else {
+    Serial.println("ERR");
+    Serial.println("No arguments");
+  }
+  
+  Serial.print("Current position ");
+  Serial.println(stepperMotor.currentPos);
   Serial.println("DONE");
-
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+void sayHello() {
+  char *arg;
+  arg = sCmd.next();    // Get the next argument from the SerialCommand object buffer
+  if (arg != NULL) {    // As long as it existed, take it
+    Serial.print("Hello ");
+    Serial.println(arg);
+  }
+  else {
+    Serial.println("Hello, whoever you are");
+  }
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////
